50b104f200542e8573e177ebf00b5e158c3b057b
vuongxuanhung <vuong.xuan.hung@framgia.com>
2015-03-02 10:02:55 +0700

    Upload image with cloudinary

--- Gemfile
+++ Gemfile
@@ -15,7 +15,9 @@ gem 'faker'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
 gem 'carrierwave'
-gem 'mini_magick'
+# gem 'mini_magick'
+# gem 'fog'
+gem 'cloudinary'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,8 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
-  
   default_scope -> {order(created_at: :desc)}
+  mount_uploader :picture, PictureUploader
 
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 200 }
